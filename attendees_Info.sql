8f7ecda2ca94b7e0d2093f892d399af9c48a7dd9
Kenneth James <kjdayapan@gmail.com>
2024-05-25 23:34:42 +0800

    Added SQL Script for DB

---
+++ attendees_Info.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[member_Info]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [PhoneNumber] INT NOT NULL, 
+    [Address] VARCHAR(MAX) NOT NULL
+)
